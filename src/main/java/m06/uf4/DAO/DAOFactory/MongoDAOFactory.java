4944fee92792004f5cea99ebda447d8dd2869310
Pau Garrido <pau.garrido.7e3@itb.cat>
2021-05-03 01:00:18 +0200

    Mongo Update

--- src/main/java/m06/uf4/DAO/DAOFactory/MongoDAOFactory.java
+++ src/main/java/m06/uf4/DAO/DAOFactory/MongoDAOFactory.java
@@ -7,9 +7,13 @@ import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import m06.uf4.DAO.comanda.Comanda;
 import m06.uf4.DAO.comanda.ComandaDAO;
+import m06.uf4.DAO.comanda.implementacio.ComandaImpMongo;
 import m06.uf4.DAO.empleat.EmpleatDAO;
+import m06.uf4.DAO.empleat.implementacio.EmpleatImpMongo;
 import m06.uf4.DAO.producte.ProducteDAO;
+import m06.uf4.DAO.producte.implementacio.ProducteImpMongo;
 import m06.uf4.DAO.proveidor.ProveidorDAO;
+import m06.uf4.DAO.proveidor.implementacio.ProveidorImpMongo;
 import org.bson.Document;
 import org.bson.codecs.configuration.CodecRegistry;
 import org.bson.codecs.pojo.PojoCodecProvider;
@@ -44,21 +48,21 @@ public class MongoDAOFactory extends DAOFactory{
 
     @Override
     public ComandaDAO getComandaDAO() {
-        return null;
+        return new ComandaImpMongo();
     }
 
     @Override
     public ProducteDAO getProducteDAO() {
-        return null;
+        return new ProducteImpMongo();
     }
 
     @Override
     public ProveidorDAO getProveidorDAO() {
-        return null;
+        return new ProveidorImpMongo();
     }
 
     @Override
     public EmpleatDAO getEmpleatDAO() {
-        return null;
+        return new EmpleatImpMongo();
     }
 }
